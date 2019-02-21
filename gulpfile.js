f8f9395fc3ffb4fa99bad68017df316c4dfd13f5
Guillaume Duclos <guillaume.duclos@idix.fr>
2019-02-21 13:59:40 +0100

    CSS config created

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+// Dependencies
+const gulp         = require('gulp'),
+      sass         = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      cleanCSS     = require('gulp-clean-css'),
+      sourcemaps   = require('gulp-sourcemaps');
 
-exports.default = defaultTask;
+// SASS tasks
+sass.compiler = require('node-sass');
+
+gulp.task('sass', () => {
+  return gulp.src('./src/styles/scss/index.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./src/styles/css'));
+});
+
+gulp.task('sass:watch', () => {
+  gulp.watch('./src/styles/scss/*.scss', ['sass']);
+});
