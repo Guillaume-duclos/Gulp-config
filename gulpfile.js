271a2544de7c6b9838263cf4fd3cce2c8270b1f2
Guillaume Duclos <guillaume.duclos@idix.fr>
2019-02-26 09:07:56 +0100

    Update config

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,7 @@ const gulp         = require('gulp'),
 
 sass.compiler = require('node-sass');
 
-gulp.task('build', () => {
-  return gulp.src('./src/styles/scss/index.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./src/styles/css'));
-});
-
+// Watching changes
 gulp.task('watch', () => {
   return watch('./src/styles/scss/*.scss', () => {
     gulp.src('./src/styles/scss/index.scss')
@@ -32,11 +23,19 @@ gulp.task('watch', () => {
   })
 });
 
-gulp.task('minify', () => {
+// Build CSS - Auto prefixing & minify
+gulp.task('build-css', () => {
+  return gulp.src('./src/styles/scss/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./build/styles'));
+});
+
+// Build JS - ES6+ & minify
+gulp.task('build-js', () => {
   gulp.src('./src/js/*.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
+    .pipe(babel({presets: ['@babel/env']}))
     .pipe(minify({
       noSource: true,
       ext: {min: '.js'},
